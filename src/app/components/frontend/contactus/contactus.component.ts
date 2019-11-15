8611b478dcca804fe3b23392799158dd55c0e0bb
amitavainfluxiq <amitava.involutiontech@gmail.com>
2019-11-15 18:34:50 +0530

    update

--- src/app/components/frontend/contactus/contactus.component.ts
+++ src/app/components/frontend/contactus/contactus.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/api.service';
 import { MetaService } from '@ngx-meta/core';
+import {ApiService} from "../../../api.service";
 
 @Component({
   selector: 'app-contactus',
@@ -8,20 +8,22 @@ import { MetaService } from '@ngx-meta/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent implements OnInit {
-  /*public formTitle: any = "Contact Us";   */   // Enter the Forl Title
+  public formTitle: any = "";      // Enter the Forl Title
   public pageUrl: any = 'home';
-  // public serverUrl: any = 'https://o820cv2lu8.execute-api.us-east-2.amazonaws.com/production/api/';
+  public modaleLogo: any = '';
+  public serverUrl: any = this.ApiService.serverUrlDemo ;
 
 
   public addEndpoint: any = {
     endpoint:'addorupdatedata',
-    source:'contactusForm'
+    source:'contactusForm',
+    token:1
   };
   public getDataUrl: any = 'datalist';
   public routeingUrl: any = 'dashboard';
   constructor(private readonly meta: MetaService, public ApiService: ApiService) {
     window.scrollTo(500, 0);
-    this.meta.setTitle('Contact Us dynamic');
+    this.meta.setTitle('Grace Medical');
     this.meta.setTag('og:description', 'This is dynamic decription ');
     this.meta.setTag('og:title', 'This is dynamic title with meta og ');
     this.meta.setTag('og:type', 'website');
