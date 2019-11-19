eb43e82b15504989a50b1a7068bcac7b72c4b237
amitavainfluxiq <amitava.involutiontech@gmail.com>
2019-11-19 18:42:37 +0530

    amitava 19-11-2019

--- src/app/components/frontend/videogallery/videogallery.component.ts
+++ src/app/components/frontend/videogallery/videogallery.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+
+export interface DialogData {
+  // animal: string;
+  // name: string;
+}
+
 @Component({
   selector: 'app-videogallery',
   templateUrl: './videogallery.component.html',
@@ -7,9 +15,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideogalleryComponent implements OnInit {
 
-  constructor() { window.scrollTo(500, 0);}
+  constructor(public dialog: MatDialog) { window.scrollTo(500, 0);}
+
+  openDialog(): void {
+
+
+    const dialogRef = this.dialog.open(VideoZoom, {
+      // width: '250px',
+      // data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      //this.animal = result;
+    });
+  }
 
   ngOnInit() {
+  }
+
+}
+
+
+@Component({
+  selector: 'videozoom',
+  templateUrl: 'videozoom.html',
+})
+export class VideoZoom {
+
+  constructor(
+      public dialogRef: MatDialogRef<VideoZoom>,
+      /* @Inject(MAT_DIALOG_DATA) public data: DialogData*/) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
